05708a0d0ca05fe5f5ca4c0cb1e9bebb63fe6f10
ESther Ng'ang'a <esther.nganga@student.moringaschool.com>
2022-10-22 17:49:16 +0300

    completed assignment

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,11 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+#def parrot()
+        #puts "Squawk!"
+#end 
+
+def parrot(sound ="")
+    puts sound =="" ? "Squawk!": sound
+    sound =="" ? "Squawk!": sound
+end
+"Squawk!"
